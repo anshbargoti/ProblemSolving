da74a7049d2b5a65520e26f13432102b8ce19fdd
Ansh Bargoti <anshbargoti@gmail.com>
2020-10-20 20:42:00 +0530

    Add solution for problem `A - Boring Apartments` Link: https://codeforces.com/contest/1433/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1433/submission/96119479

--- codeforces/1433/A.cpp
+++ codeforces/1433/A.cpp
@@ -1,29 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
- 
+
 void striker()
 {
     int x;
     cin>>x;
     int digits=log10(x)+1;
-    int count=(x%10-1)*10;
-    if(digits==1)
-        count+=1;
-    else if(digits==2)
-        count+=3;
-    else if(digits==3)
-        count+=6;
-    else
-        count+=10;
+    long count=(x%10-1)*10+digits*(digits+1)/2;
     cout<<count;
 }
- 
+
 int32_t main(){
- 
+
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
- 
+
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
