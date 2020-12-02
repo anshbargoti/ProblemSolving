0d2526a9d9bda255b3a159b4cd227da2f97068cc
Ansh Bargoti <anshbargoti@gmail.com>
2020-12-02 14:54:00 +0530

    Add solution for problem `A - Beautiful Year` Link: https://codeforces.com/contest/271/problem/A Tags: brute force, *800 Ref: https://codeforces.com/contest/271/submission/100174323

---
+++ codeforces/271/A.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+//#define int long long
+
+bool distinct_digit(int i,map<int,int> visited)
+{
+    visited[i%10]++;
+    i=i/10;
+    visited[i%10]++;
+    i=i/10;
+    visited[i%10]++;
+    i=i/10;
+    visited[i%10]++;
+    for(auto it:visited)
+    {
+        if(it.second>1)
+            return false;
+    }
+    return true;
+}
+
+void striker()
+{
+    map<int,int> visited;
+    int year;
+    cin>>year;
+    int i=year+1;
+    while(true)
+    {
+        if(distinct_digit(i,visited))
+        {
+            cout<<i;
+            return;
+        }
+        i++;
+    }
+}
+
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    
+    int t=1;
+//    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        //cout<<"#Case: "<<i+1<<endl;
+        striker();
+        cout<<endl;
+    }
+     return 0;
+}
