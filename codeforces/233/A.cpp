e737688f93547fa1322e31d70f720b9d36548c17
Ansh Bargoti <anshbargoti@gmail.com>
2020-12-03 21:32:00 +0530

    Add solution for problem `A - Perfect Permutation` Link: https://codeforces.com/contest/233/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/233/submission/100288670

---
+++ codeforces/233/A.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// const int MOD = 1000000007;
+//#define int long long
+
+
+void striker()
+{
+    int n;
+    cin>>n;
+    if(n&1)
+    {
+        cout<<-1;
+        return;
+    }
+    for(int i=n;i>0;i--)
+        cout<<i<<" ";
+}
+
+
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    
+    int t=1;
+//    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        //cout<<"#Case: "<<i+1<<endl;
+        striker();
+        cout<<endl;
+    }
+     return 0;
+}
