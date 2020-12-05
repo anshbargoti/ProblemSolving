49d1c7827522ef14797260cc4be3f1691871cb85
Ansh Bargoti <anshbargoti@gmail.com>
2020-12-05 09:41:00 +0530

    Add solution for problem `A - Amusing Joke` Link: https://codeforces.com/contest/141/problem/A Tags: implementation, sortings, strings, *800 Ref: https://codeforces.com/contest/141/submission/100414456

--- codeforces/141/A.cpp
+++ codeforces/141/A.cpp
@@ -11,7 +11,7 @@ void striker()
     s1=s1+s2;
     sort(s1.begin(),s1.end());
     sort(s3.begin(),s3.end());
-    if(s1.compare(s3)==0)
+    if(s1==s3)
         cout<<"YES";
     else
         cout<<"NO";
