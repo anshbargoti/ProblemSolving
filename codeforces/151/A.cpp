a210cb328f1efd4ce11529f96a65f2f9afab647e
Ansh Bargoti <anshbargoti@gmail.com>
2020-12-05 10:40:00 +0530

    Add solution for problem `A - Soft Drinking` Link: https://codeforces.com/contest/151/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/151/submission/100416720

---
+++ codeforces/151/A.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//#define int long long
+// const int MOD = 1000000007;
+
+void striker()
+{
+    int n,k,l,c,d,p,nl,np;
+    cin>>n>>k>>l>>c>>d>>p>>nl>>np;
+    int toast=(k*l)/nl;
+    int slice=c*d;
+    int salt=p/np;
+    cout<<min(toast,min(slice,salt))/n;
+}
+
+
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    
+    int t=1;
+//    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        //cout<<"#Case: "<<i+1<<endl;
+        striker();
+        cout<<endl;
+    }
+     return 0;
+}
