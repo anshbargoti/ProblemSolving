8e7e967e14b903b796151f0652886d37cb4bfd47
Ansh Bargoti <anshbargoti@gmail.com>
2020-12-13 22:23:00 +0530

    Add solution for problem `B - Easy Number Challenge` Link: https://codeforces.com/contest/236/problem/B Tags: implementation, number theory, *1300 Ref: https://codeforces.com/contest/236/submission/101119576

--- codeforces/236/B.cpp
+++ codeforces/236/B.cpp
@@ -2,41 +2,29 @@
 using namespace std;
 #define int long long
 const int MOD = 1073741824;
-int total_divisor[1000001];
-
-int count_divisor(int n) 
-{
-    if(total_divisor[n]!=0)
-        return total_divisor[n];
-    
-    int count=0;
-    for(int i=1;i<=sqrt(n);i++) 
-    { 
-        if(n%i==0) 
-        {
-            if(n/i==i) 
-                count++;
-            else
-                count+=2;
-        } 
-    }
-    total_divisor[n]=count;
-    return count;
-}
 
 void striker()
 {
     int a,b,c;
     cin>>a>>b>>c;
+    int count_div[1000001]={0};
+    for(int i=1;i<=a*b*c;i++)
+    {
+        for(int j=i;j<=a*b*c;j+=i)
+        {
+            count_div[j]++;
+        }
+    }
+
     int sum=0;
+
     for(int i=1;i<=a;i++)
     {
         for(int j=1;j<=b;j++)
         {
             for(int k=1;k<=c;k++)
             {
-                sum=(sum+count_divisor(i*j*k));
-                sum=sum%MOD;
+                sum+=(count_div[i*j*k])%MOD;
             }
         }
     }
