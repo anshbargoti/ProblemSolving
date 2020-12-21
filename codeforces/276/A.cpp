1533734896fbcf30982908a4bc26841d0ada4967
Ansh Bargoti <anshbargoti@gmail.com>
2020-12-21 20:36:00 +0530

    Add solution for problem `A - Lunch Rush` Link: https://codeforces.com/contest/276/problem/A Tags: implementation, *900 Ref: https://codeforces.com/contest/276/submission/101972918

---
+++ codeforces/276/A.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//#define int long long
+//const int MOD = 1000000007;
+
+
+void striker()
+{
+    int n, k;
+    cin >> n >> k;
+    int joy = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        int f, t;
+        cin >> f >> t;
+        if (t > k)
+            joy = max( joy , f-(t-k) );
+        else
+            joy = max( joy , f );
+    }
+    cout << joy;
+}
+
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    
+    int t=1;
+//    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        //cout<<"#Case: "<<i+1<<endl;
+        striker();
+        cout<<endl;
+    }
+     return 0;
+}
