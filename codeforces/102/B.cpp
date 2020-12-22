425417e87fad221827e923e2c613355eef9a6756
Ansh Bargoti <anshbargoti@gmail.com>
2020-12-22 21:51:00 +0530

    Add solution for problem `B - Sum of Digits` Link: https://codeforces.com/contest/102/problem/B Tags: implementation, *1000 Ref: https://codeforces.com/contest/102/submission/102059352

--- codeforces/102/B.cpp
+++ codeforces/102/B.cpp
@@ -15,20 +15,15 @@ void striker()
         cout << 0;
         return;
     }
-    int count_tot = 0;
-    for (char x : str)
-        count_tot += (x - '0');
-    int res = 1;
-    while (count_tot > 9)
+    int res = 0, count_tot;
+    while (n != 1)
     {
-        int temp = 0;
-        while (count_tot > 0)
-        {
-            temp += count_tot % 10;
-            count_tot /= 10;
-        }
-        ++res;
-        count_tot = temp;
+        count_tot = 0;
+        for (int i = 0; i < n; i++)
+            count_tot += str[i] - '0';
+        str = to_string(count_tot);
+        res++;
+        n = str.size();
     }
     cout << res;
 }
