c0aff62728800fdbf333bbcc2207898bcbaca301
Ansh Bargoti <anshbargoti@gmail.com>
2020-12-27 23:42:00 +0530

    Add solution for problem `B - Fixed Points` Link: https://codeforces.com/contest/347/problem/B Tags: brute force, implementation, math, *1100 Ref: https://codeforces.com/contest/347/submission/102492949

---
+++ codeforces/347/B.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+//#define int long long
+//const int MOD = 1000000007;
+
+
+void striker()
+{
+    int n;
+    cin >> n;
+    int count = 0, swap = 0, arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == i)
+            count++;
+        else if (arr[arr[i]] == i)
+            swap = 1;
+    }
+    if (count == n)
+        cout << count;
+    else if (swap)
+        cout << count + 2;
+    else
+        cout << count + 1;
+}
+
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    
+    int t = 1;
+//    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        //cout<<"#Case: "<<i+1<<endl;
+        striker();
+        cout<<endl;
+    }
+     return 0;
+}
