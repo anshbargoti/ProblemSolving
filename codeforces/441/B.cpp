7db7d856f688adc403f5776b9ad281a8f53a6759
Ansh Bargoti <anshbargoti@gmail.com>
2020-12-31 21:47:00 +0530

    Add solution for problem `B - Valera and Fruits` Link: https://codeforces.com/contest/441/problem/B Tags: greedy, implementation, *1400 Ref: https://codeforces.com/contest/441/submission/102925243

---
+++ codeforces/441/B.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+int arr[3005];
+
+//#define int long long
+//const int MOD = 1000000007;
+
+
+void striker()
+{
+    int n, v;
+    cin >> n >> v;
+    for (int i = 1; i <= n; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        arr[x] += y;
+    }
+    int ans = 0, temp, curr = v;
+    for (int i = 1; i <= 3001; i++)
+    {
+        if (arr[i - 1])
+        {
+            temp = min (curr, arr[i - 1]);
+            ans += temp;
+            curr -= temp; 
+        }
+        if (curr)
+        {
+            temp = min (curr, arr[i]);
+            ans += temp;
+            arr[i] -= temp;
+        }
+        curr = v;
+    }
+    cout << ans;
+}
+
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    
+    int t = 1;
+//    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        //cout<<"#Case: "<<i+1<<endl;
+        striker();
+        cout<<endl;
+    }
+     return 0;
+}
