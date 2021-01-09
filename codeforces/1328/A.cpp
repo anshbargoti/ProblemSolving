2fc3cc68cb82c3812f9fdbb7f3fe55aa0d95a55a
Ansh Bargoti <anshbargoti@gmail.com>
2021-01-09 23:26:00 +0530

    Add solution for problem `A - Divisibility Problem` Link: https://codeforces.com/contest/1328/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1328/submission/103905533

---
+++ codeforces/1328/A.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define end "\n"
+#define int long long
+
+
+//const int MOD = 1000000007;
+
+
+void striker()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << (a % b == 0 ? 0 : b - a % b);
+}
+
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    clock_t tStart = clock();
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i++)
+    {
+        //cout<<"#Case: "<<i+1<<endl;
+        striker();
+        cout<<end;
+    }
+
+#ifndef ONLINE_JUDGE
+    printf("\nRun Time -> %.10fs\n", (double)(clock()-tStart) / CLOCKS_PER_SEC);
+#endif
+
+    return 0;
+}
