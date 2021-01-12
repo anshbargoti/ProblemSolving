5d3792b1b52caed94c7adfa5e345538aca112e31
Ansh Bargoti <anshbargoti@gmail.com>
2021-01-12 23:20:00 +0530

    Add solution for problem `A - Holiday Of Equality` Link: https://codeforces.com/contest/758/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/758/submission/104137067

---
+++ codeforces/758/A.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+//#define int long long
+// const int MOD = 1000000007;
+
+
+void striker()
+{
+    int n;
+    cin >> n;
+    int maxi = 0;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        maxi = max(arr[i], maxi);
+    }
+    int res = 0;
+    for (int i = 0; i < n; i++)
+        res += maxi - arr[i];
+    cout << res;
+}
+
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    
+    int t = 1;
+//    cin >> t;
+    for(int i=0;i<t;i++)
+    {
+        //cout<<"#Case: "<<i+1<<endl;
+        striker();
+        cout<<endl;
+    }
+     return 0;
+}
