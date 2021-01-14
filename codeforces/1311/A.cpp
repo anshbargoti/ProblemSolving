1505478633635a9bc74fadb93a5b71fe33fa54c4
Ansh Bargoti <anshbargoti@gmail.com>
2021-01-14 23:41:00 +0530

    Add solution for problem `A - Add Odd or Subtract Even` Link: https://codeforces.com/contest/1311/problem/A Tags: greedy, implementation, math, *800 Ref: https://codeforces.com/contest/1311/submission/104357283

---
+++ codeforces/1311/A.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+//#define int long long
+// const int MOD = 1000000007;
+
+
+
+void striker()
+{
+    int a, b;
+    cin >> a >> b;
+    if (a == b)
+        cout << 0;
+    else if ( (a > b && (a - b) % 2 == 0) || (b > a && (b - a) % 2 == 1) )
+        cout << 1;
+    else
+        cout << 2;
+}
+
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    
+    int t;
+    cin >> t;
+    for(int i=0;i<t;i++)
+    {
+        //cout<<"#Case: "<<i+1<<endl;
+        striker();
+        cout<<endl;
+    }
+     return 0;
+}
