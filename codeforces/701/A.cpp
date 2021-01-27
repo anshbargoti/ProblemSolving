6864bdf242fe5124859d88ea0b5ff4f2346a90c8
Ansh Bargoti <anshbargoti@gmail.com>
2021-01-27 22:47:00 +0530

    Add solution for problem `A - Cards` Link: https://codeforces.com/contest/701/problem/A Tags: greedy, implementation, *800 Ref: https://codeforces.com/contest/701/submission/105601950

---
+++ codeforces/701/A.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+
+
+// const int maxn = 2 * 1e5;
+// vector<int> prm;
+// const int MOD = 1000000007;
+
+
+
+// void sieve()
+// {
+//     bool prime[maxn + 1];
+//     memset(prime, true, sizeof(prime));
+ 
+//     for (int p = 2; p * p <= maxn; p++)
+//     {
+//         if (prime[p] == true) 
+//         {
+//             for (int i = p * p; i <= maxn; i += p)
+//                 prime[i] = false;
+//         }
+//     }
+//     for (int p = 2; p <= maxn; p++)
+//         if (prime[p])
+//             prm.push_back(p);
+// }
+
+
+
+void striker()
+{
+    int n;
+    cin >> n;
+    pair<int, int> p[n];
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        p[i].first = x;
+        p[i].second = i + 1;
+    }
+    sort(p, p + n);
+    int i = 0;
+    while (i < n / 2)
+    {
+        cout << p[i].second << " " << p[n - 1 - i].second << endl;
+        i++;
+    }
+}
+
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+
+    int t = 1;
+//    cin >> t;
+    for(int i = 0; i < t; i++)
+    {
+        //cout<<"#Case: "<<i+1<<endl;
+        striker();
+        //cout << endl;
+    }
+    return 0;
+}
